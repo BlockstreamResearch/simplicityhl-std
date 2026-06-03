d25c324f5d4cd5893de4bddb4e07e1b73e11df85
aritkulova <aritkulova.yuliia@gmail.com>
2026-06-03 20:00:18 +0300

    refactored tests for u64

--- tests/u64_test.rs
+++ tests/u64_test.rs
@@ -5,6 +5,19 @@ use simplex::transaction::{FinalTransaction, PartialInput, ProgramInput, Require
 use simplicityhl_std::artifacts::mock::u64_mock::U64MockProgram;
 use simplicityhl_std::artifacts::mock::u64_mock::derived_u64_mock::{U64MockArguments, U64MockWitness};
 
+use rand::Rng;
+
+enum FunctionToTest {
+    CheckedAdd64,
+    SafeAdd64,
+    CheckedSubtract64,
+    SafeSubtract64,
+    CheckedMultiply64,
+    SafeMultiply64,
+    CheckedDivide64,
+    SafeDivide64
+}
+
 fn get_script(context: &simplex::TestContext) -> (U64MockProgram, Script) {
     let arguments = U64MockArguments {};
 
@@ -26,7 +39,7 @@ fn fund_script(context: &simplex::TestContext) -> anyhow::Result<()> {
     Ok(()) 
 }
 
-fn spend_script(context: &simplex::TestContext) -> anyhow::Result<()> {
+fn spend_script(context: &simplex::TestContext, function_index: u8, if_test_overflow: bool, first_arg: u64, second_arg: u64, result: u64,) -> anyhow::Result<()> {
     let signer = context.get_default_signer();
     let provider = context.get_default_provider();
 
@@ -36,7 +49,7 @@ fn spend_script(context: &simplex::TestContext) -> anyhow::Result<()> {
 
     let mut ft = FinalTransaction::new();
 
-    let witness = U64MockWitness {};
+    let witness = U64MockWitness {function_index: function_index, if_test_overflow: if_test_overflow, first_arg: first_arg, second_arg: second_arg, result: result };
 
     ft.add_program_input(
         PartialInput::new(asserts_utxos[0].clone()),
@@ -51,9 +64,277 @@ fn spend_script(context: &simplex::TestContext) -> anyhow::Result<()> {
 }
 
 #[simplex::test]
-fn u64_test(context: simplex::TestContext) -> anyhow::Result<()> {
+fn u64_test_checked_add_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedAdd64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX/2);
+    let second_arg = rand::thread_rng().gen_range(0..=u64::MAX/2);
+    let result = first_arg + second_arg;
+
     fund_script(&context)?;
-    spend_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_add_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedAdd64;
+    let if_test_overflow = true;
+
+    let first_arg = u64::MAX;
+    let second_arg = rand::thread_rng().gen_range(1..=u64::MAX);
+    let result = 0;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_add_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeAdd64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX/2);
+    let second_arg = rand::thread_rng().gen_range(0..=u64::MAX/2);
+    let result = first_arg + second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_add_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeAdd64;
+    let if_test_overflow = true;
+
+    let first_arg = u64::MAX;
+    let second_arg = rand::thread_rng().gen_range(1..=u64::MAX);
+    let result = 0;
+
+    fund_script(&context)?;
+
+    let txid_result = spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result);
+
+    assert!(
+        txid_result.is_err(),
+        "Expected this test to fail but it succeeded"
+    );
+
+    let err: String = txid_result.unwrap_err().to_string();
+    assert_eq!(err, "Failed to prune program: Execution reached a pruned branch: 744339c859e7ff6f8d33f9afa73734e1c908684feedc8c4d0a6112d3bf361317");
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_subtract_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedSubtract64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX);
+    let second_arg = rand::thread_rng().gen_range(0..=first_arg);
+    let result = first_arg - second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_subtract_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedSubtract64;
+    let if_test_overflow = true;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX-1);
+    let second_arg = rand::thread_rng().gen_range(first_arg+1..=u64::MAX);
+    let result = 0;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_subtract_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeSubtract64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX);
+    let second_arg = rand::thread_rng().gen_range(0..=first_arg);
+    let result = first_arg - second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_subtract_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeSubtract64;
+    let if_test_overflow = true;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX-1);
+    let second_arg = rand::thread_rng().gen_range(first_arg+1..=u64::MAX);
+    let result = 0;
+
+    fund_script(&context)?;
+
+    let txid_result = spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result);
+
+    assert!(
+        txid_result.is_err(),
+        "Expected this test to fail but it succeeded"
+    );
+
+    let err: String = txid_result.unwrap_err().to_string();
+    assert_eq!(err, "Failed to prune program: Execution reached a pruned branch: 744339c859e7ff6f8d33f9afa73734e1c908684feedc8c4d0a6112d3bf361317");
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_multiply_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedMultiply64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=2_u64.pow(4));
+    let second_arg = rand::thread_rng().gen_range(0..=2_u64.pow(4));
+    let result = first_arg * second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_multiply_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedMultiply64;
+    let if_test_overflow = true;
+
+    let first_arg = u64::MAX;
+    let second_arg = rand::thread_rng().gen_range(2..=u64::MAX);
+    let result = 0;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_multiply_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeMultiply64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=2_u64.pow(4));
+    let second_arg = rand::thread_rng().gen_range(0..=2_u64.pow(4));
+    let result = first_arg * second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_multiply_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeMultiply64;
+    let if_test_overflow = true;
+
+    let first_arg = u64::MAX;
+    let second_arg = rand::thread_rng().gen_range(2..=u64::MAX);
+    let result = 0;
+
+    fund_script(&context)?;
+
+    let txid_result = spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result);
+
+    assert!(
+        txid_result.is_err(),
+        "Expected this test to fail but it succeeded"
+    );
+
+    let err: String = txid_result.unwrap_err().to_string();
+    assert_eq!(err, "Failed to prune program: Execution reached a pruned branch: 744339c859e7ff6f8d33f9afa73734e1c908684feedc8c4d0a6112d3bf361317");
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_divide_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedDivide64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX);
+    let second_arg = rand::thread_rng().gen_range(1..=u64::MAX);
+    let result = first_arg / second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_checked_divide_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::CheckedDivide64;
+    let if_test_overflow = true;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX);
+    let second_arg = 0;
+    let result = 0;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_divide_64_not_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeDivide64;
+    let if_test_overflow = false;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX);
+    let second_arg = rand::thread_rng().gen_range(1..=u64::MAX);
+    let result = first_arg / second_arg;
+
+    fund_script(&context)?;
+    spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result)?;
+
+    Ok(())
+}
+
+#[simplex::test]
+fn u64_test_safe_divide_64_overflow(context: simplex::TestContext) -> anyhow::Result<()> {
+    let function_index = FunctionToTest::SafeDivide64;
+    let if_test_overflow = true;
+
+    let first_arg = rand::thread_rng().gen_range(0..=u64::MAX);
+    let second_arg = 0;
+    let result = 0;
+
+    fund_script(&context)?;
+
+    let txid_result = spend_script(&context, function_index as u8, if_test_overflow, first_arg, second_arg, result);
+
+    assert!(
+        txid_result.is_err(),
+        "Expected this test to fail but it succeeded"
+    );
+
+    let err: String = txid_result.unwrap_err().to_string();
+    assert_eq!(err, "Failed to prune program: Execution reached a pruned branch: 744339c859e7ff6f8d33f9afa73734e1c908684feedc8c4d0a6112d3bf361317");
 
     Ok(())
 }
