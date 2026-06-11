fa1984e2673fad78e29ec70e4c24c7755ae1f634
aritkulova <aritkulova.yuliia@gmail.com>
2026-06-11 15:35:13 +0300

    made asserts test more detailed; added random generation of test data

---
+++ tests/helper.rs
@@ -0,0 +1,32 @@
+use rand::Rng;
+
+#[allow(dead_code)]
+pub enum IfTestOverflow {
+    NotOverflow,
+    Overflow,
+}
+
+pub fn cast_to_bool(flag: u8) -> bool {
+    flag == 1
+}
+
+#[allow(dead_code)]
+pub fn generate_uints_in_one_range(if_same: bool, min_val: u128, max_val: u128) -> (u128, u128) {
+    let some_u = rand::thread_rng().gen_range(min_val..=max_val);
+
+    if if_same {
+        (some_u, some_u)
+    } else {
+        if min_val == max_val {
+            panic!("Can not generate different values when range is one number");
+        }
+
+        let mut other_u = rand::thread_rng().gen_range(min_val..=max_val);
+
+        while other_u == some_u {
+            other_u = rand::thread_rng().gen_range(min_val..=max_val);
+        }
+
+        (some_u, other_u)
+    }
+}
