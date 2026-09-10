6a8a86b8287f167177d692ad18fb19f42be3c24d
aritkulova <aritkulova.yuliia@gmail.com>
2026-09-10 19:24:43 +0300

    added more tests to cover new u512 division functionality

--- tests/u256_mul_div_test.rs
+++ tests/u256_mul_div_test.rs
@@ -1,6 +1,7 @@
 mod common;
 
 use primitive_types::U256;
+use std::cmp::max;
 use std::ops::Div;
 
 use crate::common::core::{Expect, run};
@@ -55,7 +56,9 @@ mod u256_mul_div_test {
     use super::*;
 
     #[simplex::test]
-    fn u256_test_mul_div_256_product_is_u256(context: simplex::TestContext) -> anyhow::Result<()> {
+    fn u256_test_mul_div_256_product_fits_into_u256(
+        context: simplex::TestContext,
+    ) -> anyhow::Result<()> {
         let a = generate_u256(U256::zero(), U256::from(u128::MAX));
         let b = generate_u256(U256::zero(), U256::from(u128::MAX));
         let c = generate_u256(U256::one(), U256::MAX);
@@ -121,12 +124,58 @@ mod u256_mul_div_test {
     }
 
     #[simplex::test]
+    fn u256_test_mul_div_256_remainder_is_zero(
+        context: simplex::TestContext,
+    ) -> anyhow::Result<()> {
+        let a = generate_u256(U256::from(u128::MAX) + 1, U256::MAX);
+        let b = generate_u256(U256::from(u128::MAX) + 1, U256::MAX);
+        let c = a;
+
+        run(
+            &context,
+            program(),
+            build_witness(
+                op(FunctionToTest::MulDiv),
+                a.to_big_endian(),
+                b.to_big_endian(),
+                c.to_big_endian(),
+                Some(b.to_big_endian()),
+            ),
+            Expect::Ok,
+        )
+    }
+
+    #[simplex::test]
     fn u256_test_mul_div_256_denominator_is_u128(
         context: simplex::TestContext,
     ) -> anyhow::Result<()> {
         let a = generate_u256(U256::one(), U256::MAX);
         let b = generate_u256(U256::one(), U256::from(u128::MAX));
         let c = generate_u256(b, U256::from(u128::MAX));
+
+        let res = safe_u512_to_u256(a.full_mul(b).div(c).to_big_endian());
+
+        run(
+            &context,
+            program(),
+            build_witness(
+                op(FunctionToTest::MulDiv),
+                a.to_big_endian(),
+                b.to_big_endian(),
+                c.to_big_endian(),
+                Some(res),
+            ),
+            Expect::Ok,
+        )
+    }
+
+    #[simplex::test]
+    fn u256_test_mul_div_256_min_denom_high(context: simplex::TestContext) -> anyhow::Result<()> {
+        let pow129: U256 = U256::from(2).pow(U256::from(129));
+
+        let a = generate_u256(U256::from(u128::MAX) + 1, pow129);
+        let b = generate_u256(U256::from(u128::MAX) + 1, pow129);
+        let c = generate_u256(U256::from(u128::MAX) + 1, pow129 - 1);
 
         let res = safe_u512_to_u256(a.full_mul(b).div(c).to_big_endian());
 
@@ -159,6 +208,137 @@ mod u256_mul_div_test {
                 b.to_big_endian(),
                 c.to_big_endian(),
                 Some([0; 32]),
+            ),
+            Expect::Ok,
+        )
+    }
+
+    #[simplex::test]
+    fn u256_test_mul_div_256_algorithm_d_512_256_check(
+        context: simplex::TestContext,
+    ) -> anyhow::Result<()> {
+        let pow2_128: U256 = U256::from(u128::MAX) + 1;
+
+        let a = generate_u256(pow2_128, U256::MAX);
+        let b = generate_u256(pow2_128, U256::MAX - pow2_128);
+
+        let product = a.full_mul(b);
+        let result_high =
+            U256::from_big_endian(&(safe_u512_to_u256((product >> 256).to_big_endian())));
+
+        let c = generate_u256(max(U256::from(u128::MAX) + 1, result_high + 1), U256::MAX);
+
+        let res = safe_u512_to_u256(product.div(c).to_big_endian());
+
+        run(
+            &context,
+            program(),
+            build_witness(
+                op(FunctionToTest::MulDiv),
+                a.to_big_endian(),
+                b.to_big_endian(),
+                c.to_big_endian(),
+                Some(res),
+            ),
+            Expect::Ok,
+        )
+    }
+
+    #[simplex::test]
+    fn u256_test_mul_div_256_algorithm_d_512_256_c_is_res_high(
+        context: simplex::TestContext,
+    ) -> anyhow::Result<()> {
+        let pow2_128: U256 = U256::from(u128::MAX) + 1;
+
+        let a: U256 = generate_u256(pow2_128, U256::MAX);
+        let b = generate_u256(pow2_128, U256::MAX - pow2_128);
+
+        let product = a.full_mul(b);
+        let result_high =
+            U256::from_big_endian(&(safe_u512_to_u256((product >> 256).to_big_endian())));
+
+        let c = result_high + 1;
+
+        let res = safe_u512_to_u256(product.div(c).to_big_endian());
+
+        run(
+            &context,
+            program(),
+            build_witness(
+                op(FunctionToTest::MulDiv),
+                a.to_big_endian(),
+                b.to_big_endian(),
+                c.to_big_endian(),
+                Some(res),
+            ),
+            Expect::Ok,
+        )
+    }
+
+    #[simplex::test]
+    fn u256_test_mul_div_256_normalize_to_threshold_512_127_norm_is_1(
+        context: simplex::TestContext,
+    ) -> anyhow::Result<()> {
+        let pow2_128: U256 = U256::from(u128::MAX) + 1;
+
+        let a: U256 = generate_u256(pow2_128, U256::MAX);
+        let b = generate_u256(pow2_128, U256::MAX - pow2_128);
+
+        let product = a.full_mul(b);
+        let result_high =
+            U256::from_big_endian(&(safe_u512_to_u256((product >> 256).to_big_endian())));
+
+        let c = generate_u256(
+            max(U256::from(2).pow(U256::from(255)), result_high + 1),
+            U256::MAX,
+        );
+
+        let res = safe_u512_to_u256(product.div(c).to_big_endian());
+
+        run(
+            &context,
+            program(),
+            build_witness(
+                op(FunctionToTest::MulDiv),
+                a.to_big_endian(),
+                b.to_big_endian(),
+                c.to_big_endian(),
+                Some(res),
+            ),
+            Expect::Ok,
+        )
+    }
+
+    #[simplex::test]
+    fn u256_test_mul_div_256_normalize_to_threshold_512_127_norm_greater_than_1(
+        context: simplex::TestContext,
+    ) -> anyhow::Result<()> {
+        let a = generate_u256(U256::from(u128::MAX) + 1, U256::MAX);
+        let b = generate_u256(
+            U256::from(u128::MAX) + 1,
+            U256::from(2).pow(U256::from(192)),
+        );
+
+        let product = a.full_mul(b);
+        let result_high =
+            U256::from_big_endian(&(safe_u512_to_u256((product >> 256).to_big_endian()))) + 1;
+
+        let c = generate_u256(
+            max(U256::from(u128::MAX) + 1, result_high),
+            U256::from(2).pow(U256::from(255)),
+        );
+
+        let res = safe_u512_to_u256(product.div(c).to_big_endian());
+
+        run(
+            &context,
+            program(),
+            build_witness(
+                op(FunctionToTest::MulDiv),
+                a.to_big_endian(),
+                b.to_big_endian(),
+                c.to_big_endian(),
+                Some(res),
             ),
             Expect::Ok,
         )
