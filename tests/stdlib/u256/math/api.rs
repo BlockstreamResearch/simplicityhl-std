fa3b6959bcc808b299de7f03e9bd51c9da566e4d
Illia Kripaka <kripakaillya@gmail.com>
2026-09-18 17:59:47 +0300

    add math U256 tests

--- tests/stdlib/u256/math/api.rs
+++ tests/stdlib/u256/math/api.rs
@@ -40,3 +40,53 @@ impl TestUint for U256Wrapper {
 
 // Stamps the 22 `#[simplex::test]` entry points for U256Wrapper. Logic lives in common::uint.
 crate::uint_tests!(U256Wrapper);
+
+mod u256_math_tests_fuzz {
+    use super::*;
+
+    use crate::common::uint_fuzz::TestUintFuzz;
+    use simplex::fuzz::FuzzEngineBuilder;
+    use simplex::fuzz::proptest::prelude::any;
+    use simplex::fuzz::proptest::strategy::{BoxedStrategy, Strategy};
+
+    type U256MathFuzzEngineBuilder =
+        FuzzEngineBuilder<U256MathTestProgram, U256MathTestArguments, U256MathTestWitness>;
+
+    impl TestUintFuzz for U256Wrapper {
+        type Arguments = U256MathTestArguments;
+
+        fn arguments() -> Self::Arguments {
+            U256MathTestArguments {}
+        }
+
+        fn arb_any() -> BoxedStrategy<Self> {
+            any::<[u8; 32]>()
+                .prop_map(|bytes| U256Wrapper(U256::from_big_endian(&bytes)))
+                .boxed()
+        }
+
+        fn arb_non_zero() -> BoxedStrategy<Self> {
+            Self::arb_any()
+                .prop_map(|value| U256Wrapper(value.0.max(U256::one())))
+                .boxed()
+        }
+
+        fn arb_fitting(low: Self, high: Self) -> BoxedStrategy<Self> {
+            assert!(low <= high);
+
+            let range = high.0 - low.0;
+
+            Self::arb_any()
+                .prop_map(move |value| {
+                    if range == U256::MAX {
+                        value
+                    } else {
+                        U256Wrapper(low.0 + value.0 % (range + U256::one()))
+                    }
+                })
+                .boxed()
+        }
+    }
+
+    crate::uint_fuzz_tests!(U256Wrapper, U256MathFuzzEngineBuilder);
+}
